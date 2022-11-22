64ba87a26b14232df73c2104b0e2088afca4f934
catarinaferrei <61137065+catarinaferrei@users.noreply.github.com>
2022-11-22 12:30:53 +0000

    Create test_client.py

---
+++ test_client/test_client.py
@@ -0,0 +1,35 @@
+from minio import Minio
+from minio.error import S3Error
+
+
+def main():
+    # Create a client with the MinIO server playground, its access key
+    # and secret key.
+    client = Minio(
+        "eb222dc9-eu-de.lb.appdomain.cloud:9000",
+        access_key="minioUser",
+        secret_key="minio123",
+        secure= False,
+    )
+
+# Make 'testyaml' bucket if not exist.
+    found = client.bucket_exists("testyaml")
+    if not found:
+        client.make_bucket("testyaml")
+    else:
+        print("Bucket 'test_aml' already exists")
+
+    client.fput_object(
+        "testyaml", "test.yaml", "path/to/file",
+    )
+    print(
+        "'"path/to/file"' is successfully uploaded as "
+        "object 'test.yaml' to bucket 'test.yaml'."
+    )
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except S3Error as exc:
+        print("error occurred.", exc)
